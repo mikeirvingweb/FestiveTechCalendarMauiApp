668493c7b1e106be2b8e495d31c28967f826388a
Mike Irving <c-mirving@kahua.com>
2023-12-17 17:03:55 +0000

    Add project files.

---
+++ FestiveTechCalendarMauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FestiveTechCalendarMauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
